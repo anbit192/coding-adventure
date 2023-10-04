9f4885b12ca058269108916c37885d55cae56dd0
anbish <minhanthongminh@gmail.com>
2023-10-04 17:13:17 +0700

    test ubuntu

--- pYthon/practice/week5/file_utils.py
+++ pYthon/practice/week5/file_utils.py
@@ -7,7 +7,7 @@ def search_file(str, path):
 
     new_path = path
     for file_dir in file_list:
-        new_path += "\\" + file_dir
+        new_path += file_dir
         file = open(new_path, "r")
         temp = []
 
@@ -15,20 +15,33 @@ def search_file(str, path):
         count = 1
         for line in line_list:
             if (str in line):
-                temp.append(file_dir)
+                temp.append(new_path)
                 temp.append(line)
 
-                list_res.append(temp)
+                list_res.append(tuple(temp))
                 new_path = path
                 break
             else:
                 count = count + 1
 
+    sort_by_name(list_res)
+
     return list_res
 
 
+def sort_by_name(results):
+    for i in range(len(results)):
+        for j in range(len(results) - 1):
+            if (results[j][0] < results[j + 1][0]):
+                temp = results[j]
+                results[j] = results[j + 1]
+                results[j + 1] = temp
+
+    
+
 def main():
-    res = search_file("buoi", "C:\\Users\\Admin\\Desktop\\study\\coding-adventure\\pYthon\\practice\\week5\\txt_list")
+    res = search_file("buoi", "txt_list/")
     print(res)
+
 
 main()
