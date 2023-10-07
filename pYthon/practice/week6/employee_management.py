e3c8fea45cd39a0ee79cf47a33d414027f23a833
anbish <minhanthongminh@gmail.com>
2023-10-07 17:53:16 +0700

    employee

--- pYthon/practice/week6/employee_management.py
+++ pYthon/practice/week6/employee_management.py
@@ -2,61 +2,65 @@ class Employee:
     def __init__(self, eid, name, year, basic_salary):
         self.eid = eid
         self.name = name
-        self.year = year
-        self.basic_salary = basic_salary
+        self.year = int(year)
+        self.basic_salary = float(basic_salary)
 
-    def get_salary(self):
+    def getSalary(self):
         return self.basic_salary
 
 
 class Manager(Employee):
-    def get_salary(self):
-        return 0
+    def getSalary(self):
+        return self.basic_salary + self.basic_salary / 4
 
 
 class DataScientist(Employee):
 
     def __init__(self, eid, name, year, basic_salary, project):
         super().__init__(eid, name, year, basic_salary)
-        self.project = project
+        self.project = int(project)
 
-    def get_salary(self):
-        return self.basic_salary
+    def getSalary(self):
+        return self.basic_salary + self.basic_salary * 0.2 + self.project * 1500    
 
 
 class Developer(DataScientist):
 
-    def get_salary(self):
-        return self.basic_salary
+    def getSalary(self):
+        return self.basic_salary + self.project * 1000
 
 
-def LoadEmployeesFromFiles(file_name):
+def loadEmploysFromFile(file_name):
     file = open(file_name, "r")
-    lines = file.readlines()
+
     employees = []
 
-    i = 0
-    while (i < len(lines)):
-        id = lines[i]
-        name = lines[i + 1]
-        year = lines[i + 2]
-        salary = lines[i + 3]
+    while (True):
+        
+        id = file.readline()
+        if not id:
+            break
+        name = file.readline()
+        year = file.readline()
+        salary = file.readline()
+
         if ("DV" in id ):
-            prjs = lines[i + 4]
+            prjs = file.readline()
             dv = Developer(id, name, year, salary, prjs)
             employees.append(dv)
-            i += 4
         elif ("DS" in id):
-            prjs = lines[i + 4]
+            prjs = file.readline()
             ds = DataScientist(id, name, year, salary, prjs)
             employees.append(ds)
-            i += 4
-        else:
-            i += 3
+        elif ("M" in id):
+            manager = Manager(id, name, year, salary)
+            employees.append(manager)
+        elif ("E" in id):
+            e = Employee(id, name, year, salary)
+            employees.append(e)
 
-    print(employees)
+    return employees
         
-
 
 
 
