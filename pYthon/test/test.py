b2dcd1de1eaa76d760708b9c0ddcabf734178a7b
anbish <minhanthongminh@gmail.com>
2023-10-28 13:33:11 +0700

    update dicom

--- pYthon/test/test.py
+++ pYthon/test/test.py
@@ -1,15 +1,2 @@
-# for i in range(1, 5):
-#     if i > 3:
-#         break
-#
-#     for j in range(1, 5):
-#         print(i*j ** 2, end = " ")
-#
-#     print()
-
-x = 0.1 + 0.1 + 0.1
-y = 0.1 * 3
-z = 0.3
-
-print(x == y)
-print(x == z)
+s = "a"
+print(s.find("b"))
--- test_parallel/dicoms/test_rotate.py
+++ test_parallel/dicoms/test_rotate.py
@@ -2,20 +2,28 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
+import math
 
-path = "./JPG_IMG/D0001.dcm.jpg"
-img = plt.imread(path)
+# Create a test image
+img = np.ones((20, 30)) * 150
 
+# Rotation angle in degrees
 rotation_angle = 45
 
-(w, h) = img.shape
+# Calculate the new image dimensions
+angle_r = math.radians(rotation_angle)  # Convert to radians
+h, w = img.shape
 
-rotation_matrix = cv2.getRotationMatrix2D((w/2, h/2), rotation_angle, scale=1)
+# Calculate the coordinates of the four corners of the rotated image
+# cos_theta = abs(math.cos(angle_r))
+# sin_theta = abs(math.sin(angle_r))
+new_w = int(math.sqrt(w**2 + h**2))
+new_h = new_w
 
-rotated_img = cv2.warpAffine(img, rotation_matrix, img.shape)
+# Calculate the rotation matrix
+rotation_matrix = cv2.getRotationMatrix2D((w/2, h/2), rotation_angle, scale=1.0)
 
-
+# Warp the image
+rotated_img = cv2.warpAffine(img, rotation_matrix, (new_w, new_h))
 
 cv2.imwrite("Rotated_test.png", rotated_img)
-
-
