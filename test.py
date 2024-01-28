48b86a1cdc1e31b9cfb9ab8b4b32ed97632b894a
anbit192 <minhanthongminh@gmail.com>
2024-01-28 15:40:54 +0700

    test

--- test.py
+++ test.py
@@ -1,17 +1,12 @@
-from matplotlib import pyplot as plt
-from sklearn import datasets,tree
-from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
-import numpy as np
+from passlib.context import CryptContext
 
-iris = datasets.load_iris()
-x = iris.data
-y = iris.target
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-clf_tree = DecisionTreeRegressor()
-model = clf_tree.fit(x, y)
+hashed_pwd = pwd_context.hash("minhandz")
 
-fig = plt.figure(figsize=(25,20))
+print(pwd_context.verify("minhandz1", hashed_pwd))
 
 
-_ = tree.plot_tree(clf_tree, feature_names=iris.feature_names, class_names=iris.target_names, filled=True)
-fig.savefig("decistion_tree2.png")
+
+
+
